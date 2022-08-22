5ab11f21e480239792e3b5cdd0fdb433b7bf2c1b
MisHkat <mishkat606@gmail.com>
2022-08-22 21:13:38 +0600

    Landing Page finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Analytics from './components/Analytics';
+import LandingPage from './components/LandingPage';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <LandingPage />
+      <Analytics/>
     </div>
   );
 }
